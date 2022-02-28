a8574952821073502552e0cd61ede7c369a620f4
Aseer1109 <64312771+Aseer1109@users.noreply.github.com>
2022-03-01 03:49:11 +0530

    Add files via upload

---
+++ Assignment1/run.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# This script trains + decodes a baseline ASR system for Wolof.
+
+# initialization PATH
+. ./path.sh  || die "path.sh expected";
+# initialization commands
+. ./cmd.sh
+
+[ ! -L "steps" ] && ln -s ../wsj/s5/steps
+
+[ ! -L "utils" ] && ln -s ../wsj/s5/utils
+
+###############################################################
+#                   Configuring the ASR pipeline
+###############################################################
+stage=5    # from which stage should this script start
+nj=2        # number of parallel jobs to run during training
+test_nj=2    # number of parallel jobs to run during decoding
+# the above two parameters are bounded by the number of speakers in each set
+###############################################################
+
+# Stage 1: Prepares the train/dev data. Prepares the dictionary and the
+# language model.
+if [ $stage -le 1 ]; then
+  echo "Preparing lexicon and language models"
+  local/prepare_lexicon.sh
+  local/prepare_lm.sh
+fi
+
+# Feature extraction
+# Stage 2: MFCC feature extraction + mean-variance normalization
+if [ $stage -le 2 ]; then
+   for x in train dev test; do
+      steps/make_mfcc.sh --nj 8 --cmd "$train_cmd" data/$x exp/make_mfcc/$x mfcc
+      steps/compute_cmvn_stats.sh data/$x exp/make_mfcc/$x mfcc
+   done
+fi
+
+# Stage 3: Training and decoding monophone acoustic models
+if [ $stage -le 3 ]; then
+  ### Monophone
+    echo "Monophone training"
+	steps/train_mono.sh --nj "$nj" --cmd "$train_cmd" data/dev lang exp/mono
+    echo "Monophone training done"
+    (
+    echo "Decoding the test set"
+    utils/mkgraph.sh lang exp/mono exp/mono/graph
+  
+    # This decode command will need to be modified when you 
+    # want to use tied-state triphone models 
+    steps/decode.sh --nj $test_nj --cmd "$decode_cmd" \
+      exp/mono/graph data/test exp/mono/decode_test
+    echo "Monophone decoding done."
+    ) &
+fi
+
+# Stage 4: Training tied-state triphone acoustic models
+if [ $stage -le 4 ]; then
+  ### Triphone
+    echo "Triphone training"
+    steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+       data/dev lang exp/mono exp/mono_ali
+	  steps/train_deltas.sh --boost-silence 1.25  --cmd "$train_cmd"  \
+	     5000 5000 data/dev lang exp/mono_ali exp/tri1
+    echo "Triphone training done"
+	  #Add triphone decoding steps here #
+    (
+    echo "Decoding the test set"
+    utils/mkgraph.sh lang exp/tri1 exp/tri1/graph
+  
+    # This decode command will need to be modified when you 
+    # want to use tied-state triphone models 
+    steps/decode.sh --nj $test_nj --cmd "$decode_cmd" \
+      exp/tri1/graph data/test exp/tri1/decode_test
+    echo "Triphone decoding done."
+    ) &
+
+fi
+
+
+# # Stage 5
+if [ $stage -le 5 ]; then
+    # echo "Data Augmentation"
+    utils/data/perturb_data_dir_speed_3way.sh data/train data/train_sp3
+    
+    utils/data/perturb_data_dir_speed_3way.sh data/dev data/dev_sp3
+
+    utils/data/perturb_data_dir_speed_3way.sh data/test data/test_sp3
+
+  ### Monophone
+    echo "Monophone training"
+	  steps/train_mono.sh --nj "$nj" --cmd "$train_cmd" data/dev_sp3 lang exp/mono_sp3
+    echo "Monophone training done"
+    (
+    echo "Decoding the test set"
+    utils/mkgraph.sh lang exp/mono_sp3 exp/mono_sp3/graph
+  
+    # This decode command will need to be modified when you 
+    # want to use tied-state triphone models 
+    steps/decode.sh --nj $test_nj --cmd "$decode_cmd" \
+      exp/mono_sp3/graph data/test_sp3 exp/mono_sp3/decode_test
+    echo "Monophone decoding done."
+    ) &
+
+      ### Triphone
+    echo "Triphone training"
+    steps/align_si.sh --nj $nj --cmd "$train_cmd" \
+       data/dev_sp3 lang exp/mono_sp3 exp/mono_ali_sp3
+	  steps/train_deltas.sh --boost-silence 1.25  --cmd "$train_cmd"  \
+	     5000 5000 data/dev_sp3 lang exp/mono_ali_sp3 exp/tri1_sp3
+    echo "Triphone training done"
+	  #Add triphone decoding steps here #
+    (
+    echo "Decoding the test set"
+    utils/mkgraph.sh lang exp/tri1_sp3 exp/tri1_sp3/graph
+  
+    # This decode command will need to be modified when you 
+    # want to use tied-state triphone models 
+    steps/decode.sh --nj $test_nj --cmd "$decode_cmd" \
+      exp/tri1_sp3/graph data/test_sp3 exp/tri1_sp3/decode_test
+    echo "Triphone decoding done."
+    ) &
+fi
+
+
+
+
+wait;
+#score
+# Computing the best WERs
+for x in exp/*/decode*; do [ -d $x ] && grep WER $x/wer_* | utils/best_wer.sh; done
